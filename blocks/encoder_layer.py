0f4bb386c62f7c4a4a908ec71f3a1c0a54b8d6c4
vejaxu <wjxu29@gmail.com>
2025-01-27 11:41:56 +0800

    update blocks

--- blocks/encoder_layer.py
+++ blocks/encoder_layer.py
@@ -5,9 +5,9 @@
 """
 from torch import nn
 
-from models.layers.layer_norm import LayerNorm
-from models.layers.multi_head_attention import MultiHeadAttention
-from models.layers.position_wise_feed_forward import PositionwiseFeedForward
+from layers.layer_norm import LayerNorm
+from layers.multi_head_attention import MultiHeadAttention
+from layers.position_wise_feed_forward import PositionwiseFeedForward
 
 
 class EncoderLayer(nn.Module):
